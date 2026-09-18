786ee54229f77425eea49fea780a447013858299
FyNex <fynex.consultoria@gmail.com>
2026-09-17 22:37:48 -0300

    chore: bump version to 5.2.8 and update changelog

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ if (typeof Promise.allSettled === 'undefined') {
   };
 }
 
-const SW_VERSION = '5.2.7';
+const SW_VERSION = '5.2.8';
 const CACHE_NAME = 'confeitex-cache-v' + SW_VERSION;
 
 // Arquivos que serão cacheados na instalação do Service Worker
